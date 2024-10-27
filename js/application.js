9f8f02276fcb610bde8d334e6ff162d229e7cd93
Rayruiw <ruiw70402@gmial.com>
2024-10-27 19:50:17 +0800

    1

--- js/application.js
+++ js/application.js
@@ -1,21 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // 获取模态窗口和游戏容器的引用
   const modal = document.getElementById("startModal");
   const gameContainer = document.getElementById("gameContainer");
   const startButton = document.getElementById("startGameButton");
   const startSound = document.getElementById("start-sound");
 
-  // 点击开始游戏按钮后，显示游戏界面，隐藏模态窗口
   startButton.addEventListener("click", function () {
-    modal.style.display = "none"; // 隐藏模态窗口
+    modal.style.display = "none";
     startSound.play()
-    gameContainer.style.display = "block"; // 显示游戏界面
-    startGame(); // 调用游戏的启动逻辑
+    gameContainer.style.display = "block";
+    startGame(); 
   });
 });
 
 function startGame() {
-  // 游戏启动逻辑
   window.requestAnimationFrame(function () {
     new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
   });
